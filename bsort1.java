caf2f873b5dfe6149fa94dfd39f843d2e536aaf2
student <student@10.12.1.207>
2017-08-17 09:55:42 +0530

    hello

--- bsort1.java
+++ bsort1.java
@@ -1,1 +1,19 @@
-# bsort1
+package bsort1;
+
+public class bsort1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int[] number={12,5,9,7,32,1,5,19,13}
+ for(i=0;i<number.length;i++)
+ {
+	 for(n=0;n<number.length;n++)
+	 {
+		 int min = number[i];
+		 int max = number[i];
+		 max=min;
+		  }
+ }
+	}
+
+}
